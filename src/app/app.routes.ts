5a27cd49f75b2e0b2fca667fa60e82173ed6ccf8
Atharva Kumbhar <atharvakumbhar08@gmail.com>
2026-01-01 13:14:17 +0530

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { HomePage } from '../pages/home-page/home-page';
+import { AboutPage } from '../pages/about-page/about-page';
+import { ServicePage } from '../pages/service-page/service-page';
+import { ContactPage } from '../pages/contact-page/contact-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full',
+  },
+  {
+    path: '',
+    component: HomePage,
+  },
+  {
+    path: 'about',
+    component: AboutPage,
+  },
+  {
+    path: 'services',
+    component: ServicePage,
+  },
+  {
+    path: 'contact',
+    component: ContactPage,
+  },
+];
