fd75da5b0b1fc528828ab5da7c2261d6f9362614
Atharva Kumbhar <atharvakumbhar08@gmail.com>
2026-01-05 13:20:45 +0530

    Add scale bounce animation to service cards

--- src/pages/home-page/home-page.ts
+++ src/pages/home-page/home-page.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { Banner } from '../../components/common/banner/banner';
 import { ServiceCardContainer } from '../../components/home/service-card-container/service-card-container';
 import { Industries } from '../../components/home/industries/industries';
+import { CountUp } from '../../components/utils/count-up/count-up';
+import { PriceTable } from '../../components/home/price-table/price-table';
 
 @Component({
   selector: 'app-home-page',
-  imports: [Banner, ServiceCardContainer, Industries],
+  imports: [Banner, ServiceCardContainer, Industries, CountUp, PriceTable],
   templateUrl: './home-page.html',
   styleUrl: './home-page.css',
 })
