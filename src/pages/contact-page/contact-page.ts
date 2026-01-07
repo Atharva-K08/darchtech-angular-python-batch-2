688c01519653b48712ef05ea46482a6a9fa62f72
Atharva Kumbhar <atharvakumbhar08@gmail.com>
2026-01-07 18:46:29 +0530

    A concise description of your updates

--- src/pages/contact-page/contact-page.ts
+++ src/pages/contact-page/contact-page.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { Banner } from '../../components/common/banner/banner';
+import { ContactInfo } from '../../components/contact/contact-info/contact-info';
+import { ContactForm } from '../../components/contact/contact-form/contact-form';
 
 @Component({
   selector: 'app-contact-page',
-  imports: [],
+  imports: [Banner, ContactInfo, ContactForm],
   templateUrl: './contact-page.html',
   styleUrl: './contact-page.css',
 })
